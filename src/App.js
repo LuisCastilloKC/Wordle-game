87516f114fe1a5f4ebc0c675908de47372fb10ab
Luis Castillo <mr.luiscastillo@gmail.com>
2022-07-06 23:59:30 -0400

    add useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+const API_URL = ' https://api.frontendexpert.io/api/fe/wordle-words';
 
 function App() {
+  const [solution, setSolution] = useState('')
+  
+  useEffect(()=>{
+    const fetchWord = async () => {
+      const response = await fetch(API_URL)
+      const words = await response.json();
+      const randomWord = words[Math.floor(Math.random() * words.length)];
+      setSolution(randomWord);
+    };
+    fetchWord()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {solution}
     </div>
   );
 }
